68a4e86483d4a88787b3dcd54ba8badf3a0b9bac
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-09-14 07:35:26 +0200

    Dodanie obsługi błędu

--- src/main/java/app/model/InvoiceNotFoundAdvice.java
+++ src/main/java/app/model/InvoiceNotFoundAdvice.java
@@ -1,2 +1,18 @@
-package app.model;public class InvoiceNotFoundAdvice {
+package app.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class InvoiceNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(InvoiceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String invoiceNotFoundHandler(InvoiceNotFoundException ex) {
+        return ex.getMessage();
+    }
 }
